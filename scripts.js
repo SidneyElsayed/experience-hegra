8dce1aed9107d4bbc0cb1e2df17faba22b8c16f3
SidneyElsayed <154788807+SidneyElsayed@users.noreply.github.com>
2024-01-15 10:49:17 +0400

    playing with animations

--- scripts.js
+++ scripts.js
@@ -8,3 +8,13 @@ lightGallery(document.getElementById('lightgallery'), {
     speed: 500,
     download: false
 });
+
+//Animate on Scroll library
+AOS.init({
+// Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+offset: 150, // offset (in px) so that even though we arrive at the original location the element is not scrolling in straight away
+delay: 50, // values from 0 to 3000, with step 50ms
+duration: 1000, // values from 0 to 3000, with step 50ms
+easing: 'ease', // default easing for AOS animations
+once: false, // whether animation should happen only once - while scrolling down
+});
