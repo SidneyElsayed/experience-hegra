e83a3b9dfdc564e41304a9ea77256814122faf03
SidneyElsayed <154788807+SidneyElsayed@users.noreply.github.com>
2026-01-08 15:45:38 +0300

    added FAQ (not the greatest, but they're fine

--- scripts.js
+++ scripts.js
@@ -18,3 +18,25 @@ duration: 1000, // values from 0 to 3000, with step 50ms
 easing: 'ease', // default easing for AOS animations
 once: false, // whether animation should happen only once - while scrolling down
 });
+
+
+// accordion
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+} 
+
